51bc9101a475706b854d6f6d321146ea51385058
LSJ-90 <lsj7641148@naver.com>
2022-05-18 17:50:59 +0900

    20220518-2

--- src/main/java/com/miso/web/persistence/MybatisDao.java
+++ src/main/java/com/miso/web/persistence/MybatisDao.java
@@ -13,9 +13,11 @@ public interface MybatisDao {
 	
 	public List<String> selectJobsByDistinct();
 
-	public List<EmpVoOfMybatis> selectEmpsByDeptNo(int deptNo);
+	public List<EmpVoOfMybatis> selectEmpsByDeptNo(List<Integer> deptNos);
 	
 	public List<EmpVoOfMybatis> selectEmpsByJob(String job);
 
 	public List<EmpVoOfMybatis> searchEmpByValue(SearchValueOfMybatis value);
+
+	public void insertEmpInfo(EmpVoOfMybatis emp);
 }
