9b0fa97af4d7d4b20d2f9822040feee97259d5e2
LSJ-90 <lsj7641148@naver.com>
2022-05-24 18:16:41 +0900

    20220524-1

--- src/main/java/com/miso/web/controller/MybatisController.java
+++ src/main/java/com/miso/web/controller/MybatisController.java
@@ -9,13 +9,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.miso.web.persistence.MybatisDao;
 import com.miso.web.vo.EmpVoOfMybatis;
@@ -34,16 +31,14 @@ public class MybatisController {
 	 * @return url
 	 */
 	@RequestMapping(value = "/mybatis/mybatisTest", method = RequestMethod.GET)
-	public String mybatisTestInti(Model model) {
-		
+	public String mybatisTestInit(Model model) {
 		List<EmpVoOfMybatis> empInfos3 = mybatisDao.selectAllEmpInfos();
 		model.addAttribute("empInfos3", empInfos3);
-
 		List<Integer> deptNos = mybatisDao.selectDeptNosByDistinct();
-		List<String> jobs = mybatisDao.selectJobsByDistinct();
-		List<Integer> mgrNos = mybatisDao.selectMgrNosByDistinct();
 		model.addAttribute("deptNos", deptNos);
+		List<String> jobs = mybatisDao.selectJobsByDistinct();
 		model.addAttribute("jobs", jobs);
+		List<Integer> mgrNos = mybatisDao.selectMgrNosByDistinct();
 		model.addAttribute("mgrNos", mgrNos);
 		
 //		for (EmpVoOfMybatis empInfo : empInfos) {
@@ -68,12 +63,8 @@ public class MybatisController {
 	
 	@RequestMapping(value = "/mybatis/selectEmpByDeptNo", method = RequestMethod.GET)
 	public String selectEmpByDeptNo(@RequestParam(value="deptNo1") String deptNo, Model model) {
-		// System.out.println(deptNo);
-		
 		// TODO: 형변환 필요할까??
 		List<Integer> parseIntDeptNo = new ArrayList<Integer>();
-		List<EmpVoOfMybatis> empInfos1 = new ArrayList<EmpVoOfMybatis>();
-		
 		if (deptNo.contains(",")) {
 			String[] splitBydeptNoArray = deptNo.split(",");
 			List<String> splitBydeptNoArrayToList = Arrays.asList(splitBydeptNoArray);
@@ -81,25 +72,27 @@ public class MybatisController {
 			for (String splitBydeptNo : splitBydeptNoArrayToList) {
 				parseIntDeptNo.add(Integer.valueOf(splitBydeptNo));
 			}
+			model.addAttribute("selectedDeptNos", parseIntDeptNo);
 		} else {
 			parseIntDeptNo.add(Integer.valueOf(deptNo));
+			model.addAttribute("selectedDeptNo", deptNo);
 		}
 		
+		List<EmpVoOfMybatis> empInfos1 = new ArrayList<EmpVoOfMybatis>();
 		if (parseIntDeptNo.contains(-1)) {
 			empInfos1 = mybatisDao.selectAllEmpInfos();
 		} else {
 			empInfos1 = mybatisDao.selectEmpsByDeptNo(parseIntDeptNo);
 		}
+		model.addAttribute("empInfos1", empInfos1);
 
 		List<EmpVoOfMybatis> empInfos3 = mybatisDao.selectAllEmpInfos();
-		model.addAttribute("empInfos1", empInfos1);
 		model.addAttribute("empInfos3", empInfos3);
-		
 		List<Integer> deptNos = mybatisDao.selectDeptNosByDistinct();
-		List<String> jobs = mybatisDao.selectJobsByDistinct();
-		List<Integer> mgrNos = mybatisDao.selectMgrNosByDistinct();
 		model.addAttribute("deptNos", deptNos);
+		List<String> jobs = mybatisDao.selectJobsByDistinct();
 		model.addAttribute("jobs", jobs);
+		List<Integer> mgrNos = mybatisDao.selectMgrNosByDistinct();
 		model.addAttribute("mgrNos", mgrNos);
 		
 		return "/mybatis/mybatisTest";
@@ -107,54 +100,53 @@ public class MybatisController {
 	
 	@RequestMapping(value = "/mybatis/selectEmpByJob", method = RequestMethod.GET)
 	public String selectEmpByJob(@RequestParam(value="job1") String job, Model model) {
-		
-		List<EmpVoOfMybatis> empInfos1 = mybatisDao.selectEmpsByJob(job);
-		List<EmpVoOfMybatis> empInfos3 = mybatisDao.selectAllEmpInfos();
+		List<EmpVoOfMybatis> empInfos1 = new ArrayList<EmpVoOfMybatis>();
+		if (job.equals("")) {
+			empInfos1 = mybatisDao.selectAllEmpInfos();
+		} else {
+			empInfos1 = mybatisDao.selectEmpsByJob(job);
+		}
 		model.addAttribute("empInfos1", empInfos1);
-		model.addAttribute("empInfos3", empInfos3);
 		
+		List<EmpVoOfMybatis> empInfos3 = mybatisDao.selectAllEmpInfos();
+		model.addAttribute("empInfos3", empInfos3);
 		List<Integer> deptNos = mybatisDao.selectDeptNosByDistinct();
-		List<String> jobs = mybatisDao.selectJobsByDistinct();
-		List<Integer> mgrNos = mybatisDao.selectMgrNosByDistinct();
 		model.addAttribute("deptNos", deptNos);
+		List<String> jobs = mybatisDao.selectJobsByDistinct();
 		model.addAttribute("jobs", jobs);
+		List<Integer> mgrNos = mybatisDao.selectMgrNosByDistinct();
 		model.addAttribute("mgrNos", mgrNos);
+		model.addAttribute("selectedJob", job);
 		
 		return "/mybatis/mybatisTest";
 	}
 	
 	@RequestMapping(value = "/mybatis/selectEmpByValue", method = RequestMethod.GET)
 	public String selectEmpByValue(SearchValueOfMybatis value, Model model) {
-		// System.out.println(value.getDeptNo2());
-		// System.out.println(value.getJob2());
 		List<EmpVoOfMybatis> empInfos2 = mybatisDao.selectEmpByValue(value);
-		List<EmpVoOfMybatis> empInfos3 = mybatisDao.selectAllEmpInfos();
 		model.addAttribute("empInfos2", empInfos2);
+		List<EmpVoOfMybatis> empInfos3 = mybatisDao.selectAllEmpInfos();
 		model.addAttribute("empInfos3", empInfos3);
-		
 		List<Integer> deptNos = mybatisDao.selectDeptNosByDistinct();
-		List<String> jobs = mybatisDao.selectJobsByDistinct();
-		List<Integer> mgrNos = mybatisDao.selectMgrNosByDistinct();
 		model.addAttribute("deptNos", deptNos);
+		List<String> jobs = mybatisDao.selectJobsByDistinct();
 		model.addAttribute("jobs", jobs);
+		List<Integer> mgrNos = mybatisDao.selectMgrNosByDistinct();
 		model.addAttribute("mgrNos", mgrNos);
+		model.addAttribute("selectedValue", value);
 		
 		return "/mybatis/mybatisTest";
 	}
 	
 	@RequestMapping(value = "/mybatis/selectEmpByAjax", method = RequestMethod.POST)
 	public String selectEmpByAjax(@RequestBody SearchValueOfMybatis value, Model model) {
-		// logger.info(value.toString());
-		
 		List<EmpVoOfMybatis> empInfos3 = mybatisDao.selectEmpByValue(value);
 		model.addAttribute("empInfos3", empInfos3);
-		// logger.info(empInfos3.toString());
-		
 		List<Integer> deptNos = mybatisDao.selectDeptNosByDistinct();
-		List<String> jobs = mybatisDao.selectJobsByDistinct();
-		List<Integer> mgrNos = mybatisDao.selectMgrNosByDistinct();
 		model.addAttribute("deptNos", deptNos);
+		List<String> jobs = mybatisDao.selectJobsByDistinct();
 		model.addAttribute("jobs", jobs);
+		List<Integer> mgrNos = mybatisDao.selectMgrNosByDistinct();
 		model.addAttribute("mgrNos", mgrNos);
 		
 		return "mybatis/testAjaxView";
@@ -162,7 +154,6 @@ public class MybatisController {
 	
 	@RequestMapping(value = "/mybatis/updateEmpInfo", method = RequestMethod.POST)
 	public String updateEmpInfo(@RequestBody List<EmpVoOfMybatis> empInfos, Model model) {
-		// logger.info(empInfos.toString());
 		
 		mybatisDao.updateEmpInfo(empInfos);
 		
@@ -171,17 +162,14 @@ public class MybatisController {
 			int empNo = empInfo.getEmpNo();
 			empNos.add(empNo);
 		}
-		// logger.info(empNos.toString());
-		
 		List<EmpVoOfMybatis> empInfos3 = mybatisDao.selectEmpsByEmpNo(empNos);
 		model.addAttribute("empInfos3", empInfos3);
-		// logger.info(empInfos3.toString());
 		
 		List<Integer> deptNos = mybatisDao.selectDeptNosByDistinct();
-		List<String> jobs = mybatisDao.selectJobsByDistinct();
-		List<Integer> mgrNos = mybatisDao.selectMgrNosByDistinct();
 		model.addAttribute("deptNos", deptNos);
+		List<String> jobs = mybatisDao.selectJobsByDistinct();
 		model.addAttribute("jobs", jobs);
+		List<Integer> mgrNos = mybatisDao.selectMgrNosByDistinct();
 		model.addAttribute("mgrNos", mgrNos);
 		
 		return "mybatis/testAjaxView";
